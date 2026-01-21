5e637ed1b720ef4f4f09e70bcf45758d05e7788b
psychic-winner <mazerunnerhaze69@gmail.com>
2026-01-21 12:26:49 +0530

    Adding amazon clone website code

---
+++ script.js
@@ -0,0 +1,18 @@
+let index = 0;
+
+const slides = document.querySelector(".hero-slides");
+const totalSlides = slides.children.length;
+
+function updateSlide() {
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+    index = (index + 1) % totalSlides;
+    updateSlide();
+}
+
+function prevSlide() {
+    index = (index - 1 + totalSlides) % totalSlides;
+    updateSlide();
+}
